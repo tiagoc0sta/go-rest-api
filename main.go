377a7672cf84923e55c58e0a9266c9ecd3f43e11
Tiago Costa <tiagodpv@gmail.com>
2024-02-25 07:27:28 -0500

    sec

--- main.go
+++ main.go
@@ -2,20 +2,19 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
+
+	"github.com/tiagoc0sta/go-rest-api/models"
+	"github.com/tiagoc0sta/go-rest-api/routes"
 )
 
-func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Home Page")
-}
 
-func HandleRequest(){
-	http.HandleFunc("/", Home)
-	log.Fatal(http.ListenAndServe(":8000", nil))
-}
 
 func main() {
+	models.Personalidades = []models.Personalidade{
+		{Nome: "Nome 1", Historia: "Historia 1"},
+		{Nome: "Nome 2", Historia: "Historia 2"},
+	}
+
 	fmt.Println("Iniciando o servidor Rest com Go")
-	HandleRequest()
+	routes.HandleRequest()
 }
---
+++ routes/routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/tiagoc0sta/go-rest-api/controllers"
+)
+
+func HandleRequest() {
+	http.HandleFunc("/", controllers.Home)
+	http.HandleFunc("/api/personalidades", controllers.TodasPersonalidades)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
