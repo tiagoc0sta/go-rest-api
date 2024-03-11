5380594b41be158e2ddf9608a881a155687ac545
Tiago Costa <tiagodpv@gmail.com>
2024-03-10 21:35:45 -0400

    add mid

--- routes/routes.go
+++ routes/routes.go
@@ -6,13 +6,17 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/tiagoc0sta/go-rest-api/controllers"
+	"github.com/tiagoc0sta/go-rest-api/middleware"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
 	r.HandleFunc("/api/personalidades", controllers.CriaUmaNovaPersonalidade).Methods("Post")
+	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaUmaPersonalidade).Methods("Delete")
+	r.HandleFunc("/api/personalidades/{id}", controllers.EditaPersonalidade).Methods("Put")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
